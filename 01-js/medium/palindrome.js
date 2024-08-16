bce94ddfae572b58a2ab7413c8d33ec225f46efb
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2024-08-17 00:38:01 +0530

    Revert "File Added"
    This reverts commit cfdc1d419e6c0d89c3d5542edfb95e32da4506be.

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -1,32 +1,9 @@
 /*
   Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
-
-  Once you've implemented the logic, test your code by running
-  - `npm run test-palindrome`
 */
 
 function isPalindrome(str) {
-  let serializedStr = "";
-
-  str.toLowerCase().split("").forEach((element) => {
-    if (element != " " && !"./?<>;,[]{}()*&^%$#@S!".includes(element)) {
-      serializedStr = serializedStr + element + "";
-    }
-  });
-
-  str = serializedStr;
-
-  let left = 0;
-  let right = str.length - 1;
-  while (left < right) {
-    if (str[left] != str[right]) {
-      console.log(str[left], " ", str[right]);
-      return false;
-    }
-    left++;
-    right--;
-  }
   return true;
 }
 
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -5,25 +5,9 @@ Try running it for
 2. Sum from 1-100000
 3. Sum from 1-1000000000
 Hint - use Date class exposed in JS
+There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function Sum(n) {
-    let sum = 0;
-    for(let i = 0;i < n;i++) {
-        sum += i;
-    }
-    return sum;
-}
-
 function calculateTime(n) {
-    let timeBefore = new Date().getTime()
-    console.log("Sum from 1 to", n , ":", Sum(n), "\nTime Difference : " + (new Date().getTime() - timeBefore) , "ms")
-
     return 0.01;
 }
-
-calculateTime(100)
-calculateTime(10000)
-calculateTime(10000000)
-calculateTime(1000000000)
-calculateTime(100000000000)
